e85b692bfb9d9f21a70f3d09d9fe0ccea7bf75d2
Pronin Egor <42776347+MrZillaGold@users.noreply.github.com>
2020-09-20 01:04:28 +1000

    Checking IDs of custom emoji

--- src/embedbuilder.ts
+++ src/embedbuilder.ts
@@ -556,7 +556,7 @@ export class EmbedBuilder extends EventEmitter {
                     }
                     // Do custom emoji action
                     for (let i = 0; i < this.emojis.length; i++) {
-                        if (reaction.emoji.name === this.emojis[i].emoji)
+                        if (reaction.emoji.name === this.emojis[i].emoji || reaction.emoji.id === this.emojis[i].emoji)
                             return this.emojis[i].do(sent, page, this.emojis[i].emoji);
                     }
                 });
